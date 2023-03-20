262416637d2c005aa185c9d03a2ec5ac2f7e2dac
RealKevinPlayz <kliemoa@gmail.com>
2023-03-20 14:50:49 +0700

    Week 4 Commits : 5 Changes Desc : 1. StudentListFragment finished

--- app/src/main/java/id/ac/ubaya/informatika/adv160420078week4/view/StudentListFragment.kt
+++ app/src/main/java/id/ac/ubaya/informatika/adv160420078week4/view/StudentListFragment.kt
@@ -5,9 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import id.ac.ubaya.informatika.adv160420078week4.R
+import id.ac.ubaya.informatika.adv160420078week4.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.fragment_student_list.*
 
 class StudentListFragment : Fragment() {
+    private lateinit var viewModel: ListViewModel
+    private val studentListAdapter = StudentListAdapter(arrayListOf())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,6 +23,27 @@ class StudentListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_student_list, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+        recView.layoutManager = LinearLayoutManager(context)
+        recView.adapter = studentListAdapter
+        observeViewModel()
+    }
+
+    fun observeViewModel() {
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
+            if(it == true) {
+                recView.visibility = View.GONE
+                progressLoad.visibility = View.VISIBLE
+            } else {
+                recView.visibility = View.VISIBLE
+                progressLoad.visibility = View.GONE
+            }
+        })
+
     }
 
 }
