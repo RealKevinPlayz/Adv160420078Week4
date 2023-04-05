7d1544a2dc53b0b75821c5cf524905add519f41e
RealKevinPlayz <kliemoa@gmail.com>
2023-04-05 13:32:55 +0700

    Week 6 Commits : 1 Changes Desc : 1. util.kt added and filled 2. ic_baseline_error_24.xml added 3. build.gradle updated 4. StudentListAdapter.kt updated

--- app/src/main/java/id/ac/ubaya/informatika/adv160420078week4/view/StudentListAdapter.kt
+++ app/src/main/java/id/ac/ubaya/informatika/adv160420078week4/view/StudentListAdapter.kt
@@ -3,10 +3,13 @@ package id.ac.ubaya.informatika.adv160420078week4.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import id.ac.ubaya.informatika.adv160420078week4.R
 import id.ac.ubaya.informatika.adv160420078week4.model.Student
+import id.ac.ubaya.informatika.adv160420078week4.util.loadImage
 import kotlinx.android.synthetic.main.fragment_student_detail.view.*
 import kotlinx.android.synthetic.main.student_list_item.view.*
 
@@ -26,6 +29,10 @@ class StudentListAdapter(val studenList:ArrayList<Student>)
 
 
     override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
+        var imageView = holder.view.findViewById<ImageView>(R.id.imageView)
+        var progressBar = holder.view.findViewById<ProgressBar>(R.id.progressBar)
+        imageView.loadImage(studenList[position].photoUrl, progressBar)
+
         holder.view.txtId.text = studenList[position].id
         holder.view.txtName.text = studenList[position].name
         holder.view.btnDetail.setOnClickListener {
